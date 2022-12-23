6539be78018bee98d0a407423914b08c858efb90
Rodrigo Dantas <rodrigodantas.91@gmail.com>
2022-12-23 06:19:11 +0000

    mocked controller

--- src/main/java/dev/hustletech/gamestore/controller/HelloWorldController.java
+++ src/main/java/dev/hustletech/gamestore/controller/HelloWorldController.java
@@ -3,14 +3,14 @@ package dev.hustletech.gamestore.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
 @RestController
+@RequiredArgsConstructor
 public class HelloWorldController {
 
     @GetMapping(value = "/hello", produces = "text/plain")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
