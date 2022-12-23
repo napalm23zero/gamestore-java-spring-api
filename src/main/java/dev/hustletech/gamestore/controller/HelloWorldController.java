9b89e4dbb2cbf48e600cb493e4c753efab3fafd3
Rodrigo Dantas <rodrigodantas.91@gmail.com>
2022-12-23 04:48:49 +0000

    swagger #nowWorking

--- src/main/java/dev/hustletech/gamestore/controller/HelloWorldController.java
+++ src/main/java/dev/hustletech/gamestore/controller/HelloWorldController.java
@@ -3,6 +3,9 @@ package dev.hustletech.gamestore.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class HelloWorldController {
 
